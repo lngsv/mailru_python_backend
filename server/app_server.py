e70a8ec9d9ef31fa7d7a7df63859b929805e8ee2
Alan Gasiev <alan_gassiev@mail.ru>
2021-10-27 01:03:03 +0300

    flake8

--- server/app_server.py
+++ server/app_server.py
@@ -2,15 +2,16 @@ import datetime
 import json
 import urllib.parse
 
+
 def app(environ, start_response):
     now = datetime.datetime.now().isoformat()
     url = urllib.parse.urlunparse((
-        'http', 
-        environ['SERVER_NAME']+':'+environ['SERVER_PORT'], 
-        environ['PATH_INFO'], 
-        '', # params
-        environ['QUERY_STRING'], 
-        '' # fragment
+        'http',
+        environ['SERVER_NAME']+':'+environ['SERVER_PORT'],
+        environ['PATH_INFO'],
+        '',  # params
+        environ['QUERY_STRING'],
+        ''  # fragment
     ))
     data = dict(time=now, url=url)
     data_bin = str.encode(json.dumps(data))
@@ -19,4 +20,3 @@ def app(environ, start_response):
         ("Content-Length", str(len(data_bin))),
     ])
     return iter([data_bin])
-
